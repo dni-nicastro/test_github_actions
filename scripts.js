25937761bd728162a9e53906a1c8e8e9ec071d39
Dani <daniele.nicastro@ringieraxelspringer.ch>
2020-09-25 17:38:11 +0200

    hello

--- scripts.js
+++ scripts.js
@@ -1,7 +1,11 @@
 import http from 'k6/http';
-import { sleep } from 'k6';
+import {check, sleep } from 'k6';
 
-export default function() {
+export default function () {
     http.get('http://test.k6.io');
+    sleep(1);
+
+    let res = http.get('http://test.k6.io');
+    check(res, { 'status was 200': r => r.status === 200 });
     sleep(1);
 }
